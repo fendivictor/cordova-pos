77f7f7b051666058eafbff456bfb1c118c07a68d
Victor <fendivictor@gmail.com>
2022-02-17 06:03:28 +0700

    Update Form Produk

---
+++ www/assets/js/pages/produk/form.js
@@ -0,0 +1,135 @@
+let idPerusahaan = auth.getCookie("id_perusahaan");
+let username = auth.getCookie("username");
+let currentAction = module.readUrlParameter("action");
+let currentKodeSelected = module.readUrlParameter("kode");
+
+const formProduk = {
+  init: function() {
+    module.loadSidebar();
+    formProduk.loadKategori();
+    formProduk.loadSatuan();
+  },
+
+  loadKategori: function() {
+    $.get(`${apiUrl}api/master/Kategori/find?id_perusahaan=${idPerusahaan}`)
+    .done(function(data) {
+      $("#kategori").html(null);
+
+      $("#kategori").append(`<option value="">Pilih Kategori</option>`);
+      if (data.metadata.status != 200) {
+        return false;
+      }
+
+      data.response.forEach(function(val, i) {
+        $("#kategori").append(`<option value="${val.id}">${val.kategori}</option>`);
+      });
+    });
+  },
+
+  loadSatuan: function() {
+    $.get(`${apiUrl}api/master/Satuan/find?id_perusahaan=${idPerusahaan}`)
+    .done(function(data) {
+      $("#satuan").html(null);
+
+      $("#satuan").append(`<option value="">Pilih Satuan</option>`);
+      if (data.metadata.status != 200) {
+        return false;
+      }
+
+      data.response.forEach(function(val, i) {
+        $("#satuan").append(`<option value="${val.satuan}">${val.satuan}</option>`);
+      });
+    });
+  },
+
+  onSubmitForm: function(formData) {
+    module.blockUI();
+    module.ajaxSubmitData(
+      currentAction == 'edit' ? `${apiUrl}api/Produk/update` : `${apiUrl}api/Produk/add`,
+      formData, 
+      function(data) {
+        module.unblockUI();
+
+        if (data.metadata.status != 200) {
+          $("#DialogIconedDanger").modal("show");
+          $("#error-message").html(data.metadata.message);
+          return false;
+        }
+
+        window.location.href = "master-produk.html";
+      });
+  },
+
+  onEdit: function() {
+    if (currentAction == "edit") {
+      $.get(`${apiUrl}api/Produk/find?kode=${currentKodeSelected}&id_perusahaan=${idPerusahaan}`)
+      .done(function(data) {
+        if (data.metadata.status != 200) {
+          $("#DialogIconedDanger").modal("show");
+          $("#error-message").html(data.metadata.message);
+          return false;
+        }
+
+        $("#kode").val(data.response.kode);
+        $("#nama").val(data.response.nama);
+        $("#kategori").val(data.response.id_kategori).trigger("change");
+        $("#harga_beli").val(data.response.harga_beli);
+        $("#harga_jual").val(data.response.harga_jual);
+        $("#satuan").val(data.response.satuan).trigger("change");
+      });
+    }
+  },
+
+  onBackKeyDown: function() {
+    window.location.href = "master-produk.html";
+  }
+}
+
+document.addEventListener('deviceready', formProduk.init, false);
+document.addEventListener("backbutton", formProduk.onBackKeyDown, false); 
+
+// Event
+$(function() {
+  let forms = document.getElementsByClassName('needs-validation');
+
+  Array.prototype.filter.call(forms, function (form) {
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      form.classList.add('was-validated');
+
+      if (form.checkValidity() === false) {
+        return false;
+      }
+
+      let kode = $("#kode").val();
+      let nama = $("#nama").val();
+      let id_kategori = $("#kategori").val();
+      let kategori = $("#kategori option:selected").text();
+      let harga_beli = $("#harga_beli").val();
+      let harga_jual = $("#harga_jual").val();
+      let satuan = $("#satuan").val();
+
+      let formData = new FormData();
+      formData.append("id_perusahaan", idPerusahaan);
+      formData.append("username", username);
+      formData.append("barcode", "");
+      formData.append("kode", kode);
+      formData.append("nama", nama);
+      formData.append("id_kategori", id_kategori);
+      formData.append("kategori", kategori);
+      formData.append("harga_beli", harga_beli);
+      formData.append("harga_jual", harga_jual);
+      formData.append("satuan", satuan);
+
+      if (currentAction == 'edit') {
+        formData.append("id", currentIdSelected);
+      }
+
+      formProduk.onSubmitForm(formData);
+
+    }, false);
+  });
+
+  formProduk.onEdit();
+});
--- www/assets/js/pages/produk/index.js
+++ www/assets/js/pages/produk/index.js
@@ -1,8 +1,101 @@
+let idPerusahaan = auth.getCookie("id_perusahaan");
+let username = auth.getCookie("username");
+let state = {
+  selectedKode: null
+}
+
 const masterProduk = {
   init: function() {
     module.loadSidebar();
-    module.loadBottomMenu('product.html');
+  },
+
+  loadData: function() {
+    $.get(`${apiUrl}api/Produk/find?id_perusahaan=${idPerusahaan}`)
+    .done(function(data) {
+      $(".listview").html(null);
+
+      if (data.metadata.status != 200) {
+        $(".listview").html(`
+          <li><a href="#">Data tidak ditemukan</a></li>
+        `);
+
+        return false;
+      }
+
+      data.response.forEach(function(val, i) {
+        $(".listview").append(`<li><a href="#" data-kode="${val.kode}" class="btn-tools">${val.kode} - ${val.nama}</a></li>`);
+      }); 
+    });
+  },
+
+  removeData: function(formData) {
+    module.blockUI();
+
+    module.ajaxSubmitData(
+      `${apiUrl}api/Produk/remove/`,
+      formData,
+      function(data) {
+        module.unblockUI();
+        masterProduk.loadData();
+
+        if (data.metadata.status != 200) {
+          $("#DialogIconedDanger").modal("show");
+          $("#error-message").html(data.metadata.message);
+          return false;
+        }
+
+        toastbox("toast-success", 3000);
+        $("#success-text").html(data.metadata.message);
+      });
+  },
+
+  onBackKeyDown: function() {
+    window.location.href = "product.html";
   }
 }
 
 document.addEventListener('deviceready', masterProduk.init, false);
+document.addEventListener("backbutton", masterProduk.onBackKeyDown, false); 
+
+// Event
+$(function() {
+  masterProduk.loadData();
+});
+
+$(document).on("click", ".btn-tools", function() {
+  let kode = $(this).data("kode");
+  state.selectedKode = kode;
+
+  $("#actionSheet").modal("show");
+});
+
+$(document).on("click", ".btn-edit", function() {
+  if (state.selectedKode == null) {
+    $("#DialogIconedDanger").modal("show");
+    $("#error-message").html("Tidak ada data terpilih");
+    return false;
+  }
+
+  window.location.href = `form-produk.html?kode=${state.selectedKode}&action=edit`;
+});
+
+$(document).on("click", ".btn-delete", function() {
+  $("#modalDelete").modal("show");
+});
+
+$("#btn-confirm-delete").click(function() {
+  $("#modalDelete").modal("hide");
+
+  if (state.selectedKode == null) {
+    $("#DialogIconedDanger").modal("show");
+    $("#error-message").html("Tidak ada data terpilih");
+    return false;
+  }
+
+  let formData = new FormData();
+  formData.append("kode", state.selectedKode);
+  formData.append("id_perusahaan", idPerusahaan);
+  formData.append("username", username);
+
+  masterProduk.removeData(formData);
+});
